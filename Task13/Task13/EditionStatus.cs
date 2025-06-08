5355e4c66a55661980bfaf9b7b913a419a6d03e2
dssvis <ishkova07.2005@gmail.com>
2025-06-08 18:59:14 +0500

    13

---
+++ Task13/Task13/EditionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    public enum EditionStatus
+    {
+        InStorage,
+        ReadingRoom,
+        AtHome
+    }
+}
