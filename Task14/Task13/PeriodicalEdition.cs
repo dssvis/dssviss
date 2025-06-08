300f7ff3fa4bcec1815a5a089d2615d086e6dd76
dssvis <ishkova07.2005@gmail.com>
2025-06-08 19:04:29 +0500

    14

---
+++ Task14/Task13/PeriodicalEdition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task13;
+
+namespace Task14
+{
+    public class PeriodicalEdition : Edition
+    {
+        public string Period { get; set; }    
+        public string Type { get; set; }  
+
+        public PeriodicalEdition(string title, string authors, int year, string publisher, int inventoryNumber,
+            EditionStatus status, float price, string period, string type)
+            : base(title, authors, year, publisher, inventoryNumber, status, price)
+        {
+            Period = period;
+            Type = type;
+        }
+
+        public override string[] GetInfo()
+        {
+            var baseInfo = base.GetInfo();
+            return baseInfo.Concat(new string[]
+            {
+                $"Период выхода: {Period}",
+                $"Вид периодики: {Type}"
+            }).ToArray();
+        }
+    }
+}
---
+++ Task14/Task13/ScientificEdition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task13;
+
+namespace Task14
+{
+    public class ScientificEdition : Edition
+    {
+        public string Field { get; set; }
+        public string Annotation { get; set; }
+
+        public ScientificEdition(string title, string authors, int year, string publisher, int inventoryNumber,
+            EditionStatus status, float price, string field, string annotation)
+            : base(title, authors, year, publisher, inventoryNumber, status, price)
+        {
+            Field = field;
+            Annotation = annotation;
+        }
+
+        public override string[] GetInfo()
+        {
+            var baseInfo = base.GetInfo();
+            return baseInfo.Concat(new string[]
+            {
+                $"Область науки и техники: {Field}",
+                $"Аннотация: {Annotation}"
+            }).ToArray();
+        }
+    }
+}
